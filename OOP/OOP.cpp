d73a31ef1541bc485eea7536dead6307bbdcf672
HaritsTama <nayotama1818@gmail.com>
2024-04-22 09:09:26 +0300

    menambahkan prosedur output pada class Matakuliah yang bersifat public dan menyelesaikan class Matakuliah

--- OOP/OOP.cpp
+++ OOP/OOP.cpp
@@ -32,4 +32,10 @@ public :
 		cout << "SKS : ";
 		cin >> sks;
 	}
+	void output()
+	{
+		cout << "Kode MK : " << kodeMk << endl;
+		cout << "Nama MK : " << namaMk << endl;
+		cout << "SKS : " << sks << endl;
+	}
 };
